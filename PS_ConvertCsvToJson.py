69270a2fa60076e2c5340ad294ba28db014afef6
PRASHANT-SAWANT <PRASHANT-SAWANT>
2018-05-14 21:45:34 +0530

    This script converts Csv to Json. Might have to run in a scheduler after gathering daily data

---
+++ PS_ConvertCsvToJson.py
@@ -0,0 +1,22 @@
+import csv
+import json
+
+# object to read csv file data
+csvfile = open('/home/sawant/AbuseDataFeed.csv', 'r')
+
+# object to write data to json file
+jsonfile = open('/home/sawant/AbuseFeed.json', 'w')
+
+# Fieldnames are not needed if your csv file already has field names defined
+# fieldnames = ("$Entry_Type", "$Log_Time", "$Attacker_Ip", "$Entry_Emails", "$Log_Message", "$Deliverable",
+              #"$Days_Unresolved", "$Incidents_Reported", "$LogEvent", "$EvtLen")
+
+def WriteToJsonFile():
+    reader = csv.DictReader(csvfile)
+    # json.dumps is a function from library that already has for loop logic written for returning row for row
+    out = json.dumps([row for row in reader])
+    #print(out) For testing purpose
+    jsonfile.write(out)
+
+# function call
+WriteToJsonFile()
