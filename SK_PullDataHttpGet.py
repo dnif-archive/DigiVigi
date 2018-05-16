1c740ab45db125247d4ab5ad39a8a5dc28031b68
shreyas shripad kulkarni <shreyaskulkarni@shreyass-MacBook-Air.local>
2018-05-16 21:39:06 +0530

    Pull data from server using get API request

---
+++ SK_PullDataHttpGet.py
@@ -0,0 +1,35 @@
+import urllib2
+import requests
+import json
+import csv
+
+url = "http://haveibeenpwned.com/api/v2/breaches"
+r = requests.get(url,verify = False) 
+
+json_data = r.content # Content of response
+
+json_parsed = json.loads(json_data)
+csvout = csv.writer(open("mydata.csv", "wb+"))
+
+csvout.writerow(("Title", "Name","Domain","BreachDate","AddedDate","ModifiedDate","PwnCount","Description","IsVerified","IsFabricated","IsSensitive","IsActive","IsRetired","IsSpamList","LogoType"))
+
+for i in range(len(json_parsed)):
+	strTitle = json_parsed[i]['Title'].encode('ascii', 'ignore').decode('ascii')
+	strName = json_parsed[i]['Name'].encode('ascii', 'ignore').decode('ascii')
+	strDomain = json_parsed[i]['Domain'].encode('ascii', 'ignore').decode('ascii')
+	strBreachDate = json_parsed[i]['BreachDate'].encode('ascii', 'ignore').decode('ascii')
+	strAddedDate = json_parsed[i]['AddedDate'].encode('ascii', 'ignore').decode('ascii')
+	strModifiedDate = json_parsed[i]['ModifiedDate'].encode('ascii', 'ignore').decode('ascii')
+	strPwnCount = json_parsed[i]['PwnCount']
+	strDescription = json_parsed[i]['Description'].encode('ascii', 'ignore').decode('ascii')
+	IsVerified = json_parsed[i]['IsVerified']
+	IsFabricated = json_parsed[i]['IsFabricated']
+	IsSensitive = json_parsed[i]['IsSensitive']
+	IsActive = json_parsed[i]['IsActive']
+	IsRetired = json_parsed[i]['IsRetired']
+	IsSpamList = json_parsed[i]['IsSpamList']
+	LogoType = json_parsed[i]['LogoType'].encode('ascii', 'ignore').decode('ascii')
+	#print(json_parsed[i]['IsVerified'])
+
+	csvout.writerow((strTitle, strName,strDomain,strBreachDate,strAddedDate,strModifiedDate,strPwnCount,strDescription,IsVerified,IsFabricated,IsSensitive,IsActive,IsRetired,IsSpamList,LogoType))
+
